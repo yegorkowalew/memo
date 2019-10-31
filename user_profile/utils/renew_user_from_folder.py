b8befdb9c8fc48591ccdf613778d34299235d20e
Коваленко Ігор Сергійович <C:\Users\i.kovalenko\AppData\Roaming\The Bat!>
2019-10-31 16:47:06 +0200

    Автоматическое создание пользователя и профиля

--- user_profile/utils/renew_user_from_folder.py
+++ user_profile/utils/renew_user_from_folder.py
@@ -5,6 +5,9 @@ from django.shortcuts import render, redirect
 import logging
 logger = logging.getLogger(__name__)
 
+from user_profile.models import Profile
+from django.contrib.auth.models import User
+
 from django.conf import settings
 
 from pathlib import Path
@@ -31,20 +34,43 @@ def get_disp_no(file_path):
         logger.error("Error with parse Dispatcher No: %s" % ind)
         return None
 
-def get_names_from_path(path):
+def get_dispatchers_from_path(path):
     p = Path(path)
     dispatcher_dirs = [str(x) for x in p.iterdir() if x.is_dir()]
     dispatcher_list = [{
-        'dispatcher_full_name': get_disp_fullname(x),
-        'dispatcher_small_name': get_disp_smallname(x),
-        'dispatcher_no': get_disp_no(x),
+        'full_name': get_disp_fullname(x),
+        'small_name': get_disp_smallname(x),
+        'no': get_disp_no(x),
         } for x in dispatcher_dirs]
     return dispatcher_list
 
+def renew_profiles(path):
+    
+    for dispatcher in get_dispatchers_from_path(path):
+        # print(dispatcher['full_name'].split(' ')[1]) # Имя
+        # print(dispatcher['full_name'].split(' ')[0]) # Отчество
+        try:
+            pr = Profile.objects.get(fullname=dispatcher['full_name'])
+            # print(pr.fullname)
+            pass
+        except BaseException as ind:
+            print(ind)
+            # Создайте пользователя и сохраните его в базе данных
+            user = User.objects.create_user(dispatcher['small_name'], '', '')
+
+            # Обновите поля и сохраните их снова
+            user.first_name = dispatcher['full_name'].split(' ')[1]
+            user.last_name = dispatcher['full_name'].split(' ')[0]
+            user.profile.fullname = dispatcher['full_name']
+            user.profile.fullname_small = dispatcher['small_name']
+            user.profile.user_no = dispatcher['no']
+            user.save()
+
 def renew(request):
     # dispatcher_folder = settings.SHEDULE_FOLDER
-    for disp in get_names_from_path(settings.SHEDULE_FOLDER):
-        print(disp['dispatcher_no'])
+    # for disp in get_dispatchers_from_path(settings.SHEDULE_FOLDER):
+    #     print(disp['dispatcher_no'])
+    renew_profiles(settings.SHEDULE_FOLDER)
     return render(request, 'adm/test.html', {'records': settings.SHEDULE_FOLDER})
 
 # TODO Переделать вьюшку под обновление пользователей из имен папок
