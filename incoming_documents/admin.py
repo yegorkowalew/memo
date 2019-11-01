ebc8ad9d20862c02235e3d5a433f26bbd390c25b
Коваленко Ігор Сергійович <C:\Users\i.kovalenko\AppData\Roaming\The Bat!>
2019-11-01 16:48:22 +0200

    Добавлена модель выдачи документа.

--- incoming_documents/admin.py
+++ incoming_documents/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import DocumentDate
+from .models import DocumentDate, DocumentMust
 
 @admin.register(DocumentDate)
 class DocumentDateAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(DocumentMust)
+class DocumentMustAdmin(admin.ModelAdmin):
     pass
--- incoming_documents/models.py
+++ incoming_documents/models.py
@@ -4,19 +4,52 @@ from django.db import models
 from user_profile.models import Profile
 from on.models import Order
 
+DOCUMENT_TYPE_CHOICES = [
+    ('pickup_fact_date', 'Комплектовочные Факт Дата'),
+    ('shipping_fact_date', 'Отгрузочные Факт Дата'),
+    ('design_fact_date', 'Конструкторская документация Факт Дата'),
+    ('material_fact_date', 'Материалы Факт Дата'),
+    ('black_metal_fact_date', 'Черный метал Факт Дата'),
+    ('galvanized_fact_date', 'Оцинкованный метал Факт Дата'),
+    ('cast_iron_fact_date', 'Чугун Факт Дата'),
+]
+
 class DocumentDate(models.Model):
     """Model definition for DocumentDate."""
+# * Поля
+    dispatcher = models.ForeignKey(
+        Profile, 
+        on_delete=models.CASCADE
+    )
+   
+    order = models.ForeignKey(
+        Order, 
+        on_delete=models.CASCADE
+    )
+   
+    document_type = models.CharField(
+        max_length=40,
+        choices=DOCUMENT_TYPE_CHOICES,
+        default='pickup_fact_date',
+    )
+   
+    date = models.DateField(
+        verbose_name='Дата',
+    )
 
-    DOCUMENT_TYPE_CHOICES = [
-        ('pickup_fact_date', 'Комплектовочные Факт Дата'),
-        ('shipping_fact_date', 'Отгрузочные Факт Дата'),
-        ('design_fact_date', 'Конструкторская документация Факт Дата'),
-        ('material_fact_date', 'Материалы Факт Дата'),
-        ('black_metal_fact_date', 'Черный метал Факт Дата'),
-        ('galvanized_fact_date', 'Оцинкованный метал Факт Дата'),
-        ('cast_iron_fact_date', 'Чугун Факт Дата'),
-    ]
+# * Мета
+    class Meta:
+        """Meta definition for DocumentDate."""
 
+        verbose_name = 'документ'
+        verbose_name_plural = 'документы'
+
+    def __str__(self):
+        """Unicode representation of DocumentDate."""
+        return "%s - %s - %s" % (self.dispatcher, self.order, self.document_type)
+
+class DocumentMust(models.Model):
+    """Model definition for DocumentMust."""
 # * Поля
     dispatcher = models.ForeignKey(
         Profile, 
@@ -27,25 +60,25 @@ class DocumentDate(models.Model):
         Order, 
         on_delete=models.CASCADE
     )
+
     must = models.BooleanField(
-            verbose_name='Выдаются',
+            verbose_name='Не выдаются',
             default=True,
     )
+
     document_type = models.CharField(
         max_length=40,
         choices=DOCUMENT_TYPE_CHOICES,
         default='pickup_fact_date',
     )
-    date = models.DateField(
-        verbose_name='Дата',
-    )
+
 # * Мета
     class Meta:
-        """Meta definition for DocumentDate."""
+        """Meta definition for DocumentMust."""
 
-        verbose_name = 'документ'
-        verbose_name_plural = 'документы'
+        verbose_name = 'выдача документа'
+        verbose_name_plural = 'выдача документа'
 
     def __str__(self):
-        """Unicode representation of DocumentDate."""
+        """Unicode representation of DocumentMust."""
         return "%s - %s - %s" % (self.dispatcher, self.order, self.document_type)
